36735db648d79a00cc8ddf362c59aa21d6463493
Paulo Soares <phsoares.ita@gmail.com>
2020-06-13 07:17:55 -0300

    fixing configFilepath

--- connection.go
+++ connection.go
@@ -5,32 +5,30 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"path"
 
 	"github.com/go-pg/pg"
 	"gopkg.in/yaml.v2"
 )
 
-func loadDatabaseOptions(configPath, environment string) map[string]string {
-	filepath := path.Join(configPath, "database.yml")
-	_, err := os.Stat(filepath)
+func loadDatabaseOptions(configFile, environment string) map[string]string {
+	_, err := os.Stat(configFile)
 	if os.IsNotExist(err) {
-		log.Fatal("Could not find database config file: " + filepath)
+		log.Fatal("Could not find database config file: " + configFile)
 	}
-	data, err := ioutil.ReadFile(filepath)
+	data, err := ioutil.ReadFile(configFile)
 	if err != nil {
-		log.Fatal("Could not read database config file: " + filepath)
+		log.Fatal("Could not read database config file: " + configFile)
 	}
 	var databaseOptions map[string]map[string]string
 	err = yaml.Unmarshal(data, &databaseOptions)
 	if err != nil {
-		log.Fatal("Could not deserialize database config file: " + filepath)
+		log.Fatal("Could not deserialize database config file: " + configFile)
 	}
 	return databaseOptions[environment]
 }
 
-func LoadDatabaseConfig(configPath, environment string) *pg.Options {
-	envOptions := loadDatabaseOptions(configPath, environment)
+func LoadDatabaseConfig(configFilepath, environment string) *pg.Options {
+	envOptions := loadDatabaseOptions(configFilepath, environment)
 	address := fmt.Sprintf("%s:%s", envOptions["host"], envOptions["port"])
 	return &pg.Options{
 		Addr:     address,
@@ -44,16 +42,18 @@ type DbConnection struct {
 	Database *pg.DB
 }
 
+// Close closes connection
 func (db DbConnection) Close() {
 	db.Database.Close()
 }
 
-func CreateConnection(configPath string) *DbConnection {
+// CreateConnection opens connection with a given yml in configFilepath dir
+func CreateConnection(configFilepath string) *DbConnection {
 	env := os.Getenv("LEANMS_ENV")
 	if len(env) == 0 {
 		env = "development"
 	}
-	dbOptions := LoadDatabaseConfig(configPath, env)
+	dbOptions := LoadDatabaseConfig(configFilepath, env)
 	db := pg.Connect(dbOptions)
 	return &DbConnection{Database: db}
 }
